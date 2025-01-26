b42765a55f30986d62245aed37e176a3e80ecd1f
ralphyhlam <ralphyhlam@uchicago.edu>
2025-01-26 21:21:47 +0000

    matched all features of target

--- app.rb
+++ app.rb
@@ -4,3 +4,53 @@ require "sinatra/reloader"
 get("/") do
   erb(:index)
 end
+
+get("/rock") do 
+  moves= ["rock", "paper", "scissors"]
+  @computer= moves.sample
+  @outcome= ""
+
+  if @computer=="rock"
+    puts @outcome= "We tied!"
+  elsif @computer=="paper"
+    puts @outcome= "We lost!"
+  elsif @computer=="scissors"
+    puts @outcome= "We won!"
+  end
+
+  erb(:rock)
+end
+
+get ("/paper") do
+  moves= ["rock", "paper", "scissors"]
+  @computer= moves.sample
+  @outcome= ""
+
+  if @computer=="rock"
+    puts @outcome= "We won!"
+  elsif @computer=="paper"
+    puts @outcome= "We tied!"
+  elsif @computer=="scissors"
+    puts @outcome= "We lost!"
+  end
+
+  erb(:paper)
+
+end
+
+get ("/scissors") do
+  moves= ["rock", "paper", "scissors"]
+  @computer= moves.sample
+  @outcome= ""
+
+  if @computer=="rock"
+    puts @outcome= "We lost!"
+  elsif @computer=="paper"
+    puts @outcome= "We won!"
+  elsif @computer=="scissors"
+    puts @outcome= "We tied!"
+  end
+
+  erb(:paper)
+
+end
